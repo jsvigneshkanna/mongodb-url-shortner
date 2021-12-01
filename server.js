c86bc97fa11972f85d75a5621ff3898ca33a3db4
vigneshkanna <vkrishitaran@gmail.com>
2021-12-01 11:08:43 +0530

    views/ backend completed

--- server.js
+++ server.js
@@ -1,21 +1,63 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import ejs from "ejs";
 import { urls } from "./urlSchema.js";
 
 dotenv.config();
 const app = express();
 app.listen(process.env.PORT || 5000, () => {
-  console.log("server connected");
+  console.log("server connected in site: http://localhost:5000");
 });
 
+// Template Views engine
+app.set("view engine", ejs);
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Mongodb
 mongoose.connect(process.env.MONGO_URL);
 const dbCon = mongoose.connection;
 dbCon.on("open", () => {
   console.log("mongoose connected");
 });
 
+// Home page rendering with values from mongodb
 app.get("/", async (req, res) => {
-  const Urls = await urls.create({ fullUrl: "abcde" });
-  await Urls.save();
+  try {
+    const Urls = await urls.find();
+    res.render("index.ejs", { Urls: Urls });
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+    res.send("Something wrong 😟 check the logs!");
+  }
+});
+
+// Posting the fullurl to mongodb to store
+app.post("/url", async (req, res) => {
+  try {
+    const { fullUrl: fullurl } = req.body;
+    const url = await urls.create({
+      fullUrl: fullurl,
+    });
+    await url.save();
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Accessing the shorturl from homepage
+app.get("/:shorturl", async (req, res) => {
+  try {
+    const shorturl = req.params.shorturl;
+    let { fullUrl, clicks } = await urls.findOne({ shortUrl: shorturl });
+    console.log(fullUrl);
+    res.redirect(fullUrl);
+  } catch (error) {
+    console.error(error);
+  }
 });
